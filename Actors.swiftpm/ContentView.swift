ed1a53c9333a39ba5cf0adfeaf57a150ba714b79
Alex Brown <alexbbrown@gmail.com>
2022-07-10 22:01:47 -0700

    added readme and sample output

--- Actors.swiftpm/ContentView.swift
+++ Actors.swiftpm/ContentView.swift
@@ -3,10 +3,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Knock Knock")
+                .knocker
+            Text("Who's There")
+                .whoever
+            Text("Broken Pencil")
+                .knocker
+            Text("Broken Pencil who?")
+                .whoever
+            Text("Never mind, there's no point")
+                .knocker
         }
+    }
+}
+
+extension View {
+    var knocker: some View {
+        self
+            .font(.title)
+            .foregroundColor(.blue)
+    }
+    var whoever: some View {
+        self
+            .font(.title)
+            .foregroundColor(.yellow)
     }
 }
---
+++ Actors.swiftpm/README.swift
@@ -0,0 +1,7 @@
+/**
+
+ Test to see if I can convert an actor 'state machine' and link its async operations to a view moving through some sort of wizard.
+ 
+ Knock Knock!
+
+**/
